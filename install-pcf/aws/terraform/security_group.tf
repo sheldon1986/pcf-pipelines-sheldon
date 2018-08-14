691a880fb5b9ccb8ede9e22888cbd5f90b9fa3c5
MinSeok Kim <myminseok@gmail.com>
2018-08-15 01:19:13 +0900

    [kor] add security group opsman

--- install-pcf/aws/terraform/security_group.tf
+++ install-pcf/aws/terraform/security_group.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "directorSG" {
     ingress {
         from_port = 443
         to_port = 443
-        protocol = "HTTPS"
+        protocol = "TCP"
         cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
