5497dd5c2c60b7a6d5b581c320a59d750349a914
MinSeok Kim <myminseok@gmail.com>
2018-11-05 14:49:55 +0900

    test for vsphere

---
+++ upgrade-ops-manager/vsphere/deploy.sh
@@ -0,0 +1,1 @@
+fly -t pcfdemo sp -p upgrade-opsman -c pipeline.yml -l ../../../params-upgrade-ops-manager-vsphere.yml -l ../../../secret.yml
---
+++ upgrade-tile/upgrade-pas.sh
@@ -0,0 +1,1 @@
+fly -t pcfdemoe sp -p upgrade-pas -c pipeline.yml -l ../../params-upgrade-pas.yml -l ../../secret.yml
